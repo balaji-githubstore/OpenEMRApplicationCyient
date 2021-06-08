2018f7e7f03a103e6f6cf711ac336beca584e48b
balaji-githubstore <balaji0017@gmail.com>
2021-06-08 16:15:16 +0530

    excel read basic

--- src/test/java/com/cyient/test/DemoTest.java
+++ src/test/java/com/cyient/test/DemoTest.java
@@ -1,64 +1,34 @@
 package com.cyient.test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.cyient.page.LoginPage;
-
-//we will delete it
+//we will delete this file
 public class DemoTest {
-	// bala,bala123 --> 1 test case
-	// john,john123 --> 2 test case
-	// peter,peter123 --> 3 test case
-	
 
-	@DataProvider
-	public Object[][] validData() {
-		Object[][] main = new Object[3][2];
-		// i-->number of testdata/testcase
-		// j-->number of parameter
+	public static void main(String[] args) throws IOException {
 
-		main[0][0] = "bala";
-		main[0][1] = "bala123";
+		FileInputStream file = new FileInputStream("src/test/resources/testdata/OpenEMRData.xlsx");
 
-		main[1][0] = "Bala";
-		main[1][1] = "john123";
+		XSSFWorkbook book = new XSSFWorkbook(file); // open
+		XSSFSheet sheet = book.getSheet("validCredentialTest");
+		XSSFRow row = sheet.getRow(2);
+		XSSFCell cell = row.getCell(1);
+		
+		
+		String cellValue = cell.getStringCellValue();
+		System.out.println(cellValue);
+		
+		//rowcount
+		//cellcount
+		
 
-		main[2][0] = "Balaji";
-		main[2][1] = "peter123";
-	
-		return main;
 	}
-
-	@Test(dataProvider = "validData")
-	public void validTest(String username, String password) {
-		System.out.println("Test Run 123" + username + password);
-	}
-	
-//	  @DataProvider
-//      public Object[][] validData()
-//      {
-//              Object[][] main= new Object[2][4];
-//              main[0][0]="admin";
-//              main[0][1]="pass";
-//              main[0][2]="English (Indian)";
-//              main[0][3]="Open EMR";
-//              return main;
-//      }
-//
-//      @Test(dataProvider = "validCredentialTest")
-//      public void validCredentialTest(String username,String password,String lang,String espectedValue) {
-//              LoginPage page = new LoginPage(driver);
-//              page.sendUsername(username);
-//              page.sendPassword(password);
-//              page.selectLanguage(lang);
-//              page.selectLogin();
-//              DashBoardPage dashboard = new DashBoardPage(driver);
-//              System.out.println(dashboard.getDashBoardPageTitle());
-//
-//              Assert.assertEquals(dashboard.getDashBoardPageTitle(), espectedValue);
-//
-//      }
 
 }
